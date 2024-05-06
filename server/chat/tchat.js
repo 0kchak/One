0a226170311993e14a5bdd984e3c37e767249c92
Maya SANTINI <maya.santini@ens.uvsq.fr>
2024-05-06 17:11:43 +0200

    .env srv tcht

--- server/chat/tchat.js
+++ server/chat/tchat.js
@@ -5,7 +5,7 @@ function setupSocket(server) {
   // Paramétrage socket.io pour le serveur hebergé en local
   const io = socketIo(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.SERV_TCHAT,
       methods: ["GET", "POST"],
     },
   });
